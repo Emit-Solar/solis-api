e3a6214d3134048b4d740c5de885e59819a15116
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-03-10 15:48:19 +0000

    Checks dashboard for all stations exists

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 import collector
 import time
+from logger import logger
+import dashboard
+import parse
 
 collector.start_data_collection()
 
+sns = parse.get_all_sns()
+
+for sn in sns:
+    name = parse.get_station_name(sn)
+    db = dashboard.generate_dashboard(sn, name)
+    err = dashboard.write_dashboard(dashboard)
+
+    if err:
+        logger.error(f"Unable to create dashboard for {name}.")
+    else:
+        logger.info(f"Created dashboard for {name}.")
 
 while True:
     time.sleep(5)
